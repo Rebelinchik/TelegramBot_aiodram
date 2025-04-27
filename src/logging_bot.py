3d5c9f6ca0e6c7bff3ee6c165419e2f0d1ab322b
Anton Tarasov <4e6ypek1999@gmail.com>
2025-04-27 23:30:35 +0400

    структура бота на классе

--- src/logging_bot.py
+++ src/logging_bot.py
@@ -1,10 +1,22 @@
 import logging
 
-logging.basicConfig(
-    level=logging.INFO,
-    filename="telegrambot_aiogram",
-    filemode="a",
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    encoding="utf-8",
-)
+###создание логера
 logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+###Обработчик для файла
+file_handler = logging.FileHandler(filename="bot.log", encoding="utf-8")
+file_handler.setLevel(logging.INFO)
+
+###обработчик для консоли
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+
+###формат сообщений и присвавание формата файлу и консоли
+formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+console_handler.setFormatter(formatter)
+file_handler.setFormatter(formatter)
+
+###добавляем обработчики в логер
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
--- src/main.py
+++ src/main.py
@@ -1,37 +1,66 @@
-from aiogram import Bot, Dispatcher, types, F
-from aiogram.filters import Command
-from aiogram.fsm.context import FSMContext
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 import asyncio
-from dotenv import load_dotenv
 import os
 
-from logging_bot import *
+from aiogram import Bot, Dispatcher, F, types
+from aiogram.filters import Command
+from dotenv import load_dotenv
 
-### выгрузка токена и админа из .env
+# свои модули
+from logging_bot import logger
+from memory_bot import *
+from keyboard_bot import keyboard
+
+
+###ТОКЕН
 load_dotenv()
 TOKEN = os.getenv("TOKEN")
-ADMIN = os.getenv("ADMIN")
+if not TOKEN:
+    logger.error("TOKEN не найден в файле")
+    exit(1)
 
 
+###КЛАСС БОТА
 class TelegramBot:
-    pass
+    def __init__(self):
+        self.bot = Bot(token=TOKEN)
+        self.dp = Dispatcher()
+        ###Клавиатура
+        self.button = keyboard
+        ###Регистрация всех хендлеров
+        self.register_hundlers()
+
+    def register_hundlers(self):
+        self.dp.message.register(self.start, Command("start"))
+        self.dp.message.register(self.other_text)
+
+    ###Хендлеры
+    # Старт
+    async def start(self, message: types.Message):
+        logger.info(f"Пользователь {message.from_user.id} запустил бота через /start")
+        await message.answer(
+            f"Здравствуй {message.from_user.username}.\nПожалуйста зарегистрируйся",
+            reply_markup=self.button,
+        )
+
+    # Не обрабатываемые сообщения
+    async def other_text(self, message: types.Message):
+        logger.info(
+            f"Пользователь {message.from_user.id} отправил необрабатываемое сообщение"
+        )
+        await message.answer(f"Я обрабатываю только команды с вcтроенной клавиатуры")
+        if message.text:
+            await message.answer(f"Твоё сообщение: {message.text}")
+
+    ###Запуск бота
+    async def run(self):
+        logger.info("Бот запущен")
+        await self.dp.start_polling(self.bot)
 
 
-### инициальзация бота и диспетчера
-bot = Bot(token=TOKEN)
-dp = Dispatcher()
-
-
-### запуск бота
-async def main():
-    logger.info("Бот запущен")
-    await dp.start_polling(bot)
-
-
-try:
-    if __name__ == "__main__":
-        asyncio.run(main())
-except Exception as e:
-    print(e)
+###точка входа
+if __name__ == "__main__":
+    try:
+        bot = TelegramBot()
+        asyncio.run(bot.run())
+    except Exception as e:
+        print(e)
